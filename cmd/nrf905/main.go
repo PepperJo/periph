7b1fff802eacbf2d83f78d69439a8f79c49caa05
PepperJo <jonas1pfefferle@gmail.com>
2018-07-30 16:52:23 +0200

    nrf905 cmd

---
+++ cmd/nrf905/main.go
@@ -0,0 +1,69 @@
+// Copyright 2016 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+//go:generate go run gen.go
+
+// ssd1306 writes to a display driven by a ssd1306 controler.
+package main
+
+import (
+    "periph.io/x/periph/conn/gpio"
+    "periph.io/x/periph/conn/gpio/gpioreg"
+    "periph.io/x/periph/conn/physic"
+    "periph.io/x/periph/conn/spi"
+    "periph.io/x/periph/conn/spi/spireg"
+    "periph.io/x/periph/devices/nrf905"
+)
+
+func mainImpl() error {
+    spiID := flag.String("spi", "", "SPI port to use")
+    trxceName := flag.String("trxce", "", "TRX_CE")
+    pwrupName := flag.String("pwrup", "", "PWR_UP")
+    txenName := flag.String("txen", "", "TX_EN")
+    amName := flag.String("am", "", "AM")
+    drName := flag.String("dr", "", "DR")
+
+    verbose := flag.Bool("v", false, "verbose mode")
+    flag.Parse()
+    if !*verbose {
+        log.SetOutput(ioutil.Discard)
+    }
+    log.SetFlags(log.Lmicroseconds)
+    if flag.NArg() != 0 {
+        return errors.New("unexpected argument, try -help")
+    }
+
+    if _, err := hostInit(); err != nil {
+        return err
+    }
+
+    c, err := spireg.Open(*spiID)
+    if err != nil {
+        return err
+    }
+    defer c.Close()
+    if p, ok := c.(spi.Pins); ok {
+        log.Printf("Using pins CLK: %s  MOSI: %s MISO: %s  CS: %s", p.CLK(), p.MOSI(), p.MISO(), p.CS())
+    }
+
+    trx_en := gpioreg.ByName(*trxceName)
+    pwr_up := gpioreg.ByName(*pwrupName)
+    tx_en := gpioreg.ByName(*txenName)
+    am := gpioreg.ByName(*amName)
+    dr := gpioreg.ByName(*drName)
+
+    s, err := nrf905.New(c, trx_en, pwr_up, tx_en, am, dr)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func main() {
+    if err := mainImpl(); err != nil {
+        fmt.Fprintf(os.Stderr, "nrf905: %s.\n", err)
+        os.Exit(1)
+    }
+}
--- devices/nrf905/nrf905.go
+++ devices/nrf905/nrf905.go
@@ -3,19 +3,62 @@
 package nrf905
 
 import (
-	"periph.io/x/periph/conn"
-	"periph.io/x/periph/conn/physic"
-	"periph.io/x/periph/conn/spi"
+    "fmt"
+
+    "periph.io/x/periph/conn"
+    "periph.io/x/periph/conn/physic"
+    "periph.io/x/periph/conn/spi"
+    "periph.io/x/periph/conn/gpio"
 )
 
-func New(p spi.Port, trx_ce gpio.PinOut, pwr_up gpio.PinOut, tx_en gpio.PinOut, am gpio.PinIn, dr gpio.PinIn) {
+func New(p spi.Port, trx_ce gpio.PinOut, pwr_up gpio.PinOut, tx_en gpio.PinOut, am gpio.PinIn, dr gpio.PinIn) (*Dev, error) {
     c, err := p.Connect(10*physic.MegaHertz, spi.Mode0, 8)
     if err != nil {
         return nil, err
     }
+    if err := trx_ce.Out(gpio.Low); err != nil {
+        return nil, err
+    }
+    if err := pwr_up.Out(gpio.Low); err != nil {
+        return nil, err
+    }
+    if err := tx_en.Out(gpio.Low); err != nil {
+        return nil, err
+    }
+
+    if err := am.In(gpio.PullDown, gpio.NoEdge); err != nil {
+        return nil, err
+    }
+    if err := dr.In(gpio.PullDown, gpio.RisingEdge); err != nil {
+        return nil, err
+    }
+
+    return &Dev{c: c,
+                trx_ce: trx_ce,
+                pwr_up: pwr_up,
+                tx_en: tx_en,
+                am: am,
+                dr: dr}, nil
 }
 
 type Dev struct {
     c       conn.Conn
 
+    trx_ce  gpio.PinOut
+    pwr_up  gpio.PinOut
+    tx_en   gpio.PinOut
+
+    am      gpio.PinIn
+    dr      gpio.PinIn
+}
+
+func (d *Dev) ReadRF() error {
+    var rf [10]byte
+    if err := d.c.Tx([]byte{0x1, 0x0}, rf[:]); err != nil {
+        return err
+    }
+
+    fmt.Println("rf = ", rf)
+
+    return nil
 }
